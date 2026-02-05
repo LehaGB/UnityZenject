c3bbedc40ad754c58a8a318b514ef1ef065c8f0e
Алексей <manay.ru@mail.ru>
2026-02-06 07:42:42 +1000

    Delete

--- Zenject/UnityZenject/Assets/Scripts/Object/ICharacter.cs
+++ Zenject/UnityZenject/Assets/Scripts/Object/ICharacter.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public interface ICharacter
 {
-    
+    event Action OnDeath;
+
+    void Move(Vector3 direction, float deltaTime);
+
+    Vector3 GetPosition();
 }
